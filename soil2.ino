d1bf1ae1e0d2e965d9dadc40a53ed910885a0a62
onzverge <onzverge13@gmail.com>
2016-01-26 10:30:09 -0700

    upload

--- soil2.ino
+++ soil2.ino
@@ -11,102 +11,101 @@ rgb_lcd lcd;
 float temperature;
 int a;
 int temp;
-//float temp; //The pin number of the temperature sensor
-int B=3975;                  //B value of the thermistor
+int B = 3975;                //B value of the thermistor
 float resistance;
-int HumidityPin1=A1; //The pin number of the humidity sensor1
-int HumidityPin2=A2; //The pin number of the humidity sensor2
+int HumidityPin1 = A1; //The pin number of the humidity sensor1
+int HumidityPin2 = A2; //The pin number of the humidity sensor2
 const int relayPin =  6; //THe pin number of power relay
-IoTkit iotkit; //Intel analytics's 
+IoTkit iotkit; //Intel analytics's enable
 
 void setup() {
-Serial.begin(115200);
-lcd.begin(16, 2); //LCD begin
-pinMode(relayPin, OUTPUT); // initialize the relay pin as an output 
-iotkit.begin(); //Intel analytics's enable
+  Serial.begin(115200);
+  lcd.begin(16, 2); //LCD begin
+  pinMode(relayPin, OUTPUT); // initialize the relay pin as an output
+  iotkit.begin(); //Intel analytics's enable
 }
 
 void loop() {
 
 
-int humidityValue1; //store the analog value from humidity sensor1
-int humidityValue2; //store the analog value from humidity sensor2
-int AverageHumidity; //store average for humidity sensor1 and sensor2
+  int humidityValue1; //store the analog value from humidity sensor1
+  int humidityValue2; //store the analog value from humidity sensor2
+  int AverageHumidity; //store average for humidity sensor1 and sensor2
 
-// temperature sensor calculation from grove example
-int del=1000;
-a = analogRead(0); //read the temperature sensor
-resistance=(float)(1023-a)*10000/a; //get the resistance of the sensor;
-temperature=1/(log(resistance/10000)/B+1/298.15)-273.15;//convert to temperature via datasheet ;
-temp = temperature; 
+  // temperature sensor calculation from grove example
+  int del = 1000;
+  a = analogRead(0); //read the temperature sensor
+  resistance = (float)(1023 - a) * 10000 / a; //get the resistance of the sensor;
+  temperature = 1 / (log(resistance / 10000) / B + 1 / 298.15) - 273.15; //convert to temperature via datasheet ;
+  temp = temperature;
 
 
 
-delay(1000);
+  delay(1000);
   Serial.print("Current temperature is ");
   Serial.println(temp);
-  
-humidityValue1=analogRead(HumidityPin1); //read the humidity sensor1
-humidityValue2=analogRead(HumidityPin2); //read the humidity sensor2
 
-// Control in case one sensor is down.
-if (humidityValue1 + humidityValue2 == humidityValue1) {
-    AverageHumidity = humidityValue1;
-	}
-else if (humidityValue1 + humidityValue2 == humidityValue2) {	
-	AverageHumidity = humidityValue2;
-	}
-else {
-AverageHumidity=(humidityValue1 + humidityValue2)/2;
-}
+  humidityValue1 = analogRead(HumidityPin1); //read the humidity sensor1
+  humidityValue2 = analogRead(HumidityPin2); //read the humidity sensor2
 
-Serial.print("H1:");
-Serial.println(humidityValue1);
-Serial.print("AV:");
-Serial.println(AverageHumidity);
-Serial.print("H2:");
-Serial.println(humidityValue2);
-iotkit.send("garden", AverageHumidity);
-iotkit.send("temp", temperature);
+  // Control in case one sensor is down. Not used in latest release.
+  //if (humidityValue1 + humidityValue2 == humidityValue1) {
+  //    AverageHumidity = humidityValue1;
+  //	}
+  //else if (humidityValue1 + humidityValue2 == humidityValue2) {
+  //	AverageHumidity = humidityValue2;
+  //	}
+  //else {
+  //AverageHumidity=(humidityValue1 + humidityValue2)/2;
+  //}
+
+  Serial.print("H1:");
+  Serial.println(humidityValue1);
+  Serial.print("H2:");
+  Serial.println(humidityValue2);
+  Serial.print("AV:");
+  Serial.println(AverageHumidity);
+  iotkit.send("garden", AverageHumidity);
+  iotkit.send("temp", temperature);
   delay(1000);
 
-//LCD shows the humidity of the soil
-lcd.clear();
-lcd.setCursor(0, 0); // set the cursor to column 6, line 0
-lcd.print("H1:")+lcd.print(humidityValue1)+lcd.print(" ");
-//lcd.print("AV:")+lcd.print(AverageHumidity)+lcd.print(" ");
-lcd.print("H2:")+lcd.print(humidityValue2);
+  //LCD shows the humidity of the soil
+  lcd.clear();
+  lcd.setCursor(0, 0); // set the cursor to column 6, line 0
+  lcd.print("H1:") + lcd.print(humidityValue1) + lcd.print(" ");
+  //lcd.print("AV:")+lcd.print(AverageHumidity)+lcd.print(" ");
+  lcd.print("H2:") + lcd.print(humidityValue2);
 
-//LCD shows the temperature C
-lcd.setCursor(0, 1); // set the cursor to column 0, line 0
-lcd.print("T:");
-lcd.print(temp)+lcd.print("C");
+  //LCD shows the temperature C
+  lcd.setCursor(0, 1); // set the cursor to column 0, line 0
+  lcd.print("T:");
+  lcd.print(temp) + lcd.print("C");
 
-//Main loop
-//if (AverageHumidity<630) {
-if (humidityValue1 < 630 || humidityValue2 < 630) {  
-lcd.print("   DRY");
-lcd.setRGB(200, 0, 0); // Red background
-digitalWrite(relayPin, HIGH); // turn relay on (water pump is on)
-Serial.print("Pump is working");
-delay(20000); //pump is working for 20 sec.
-Serial.print("Pump was working for 20 sec");
-}
-// According to web reading between 300 and 600 is OK for plants.Experient shows that 600 should be a lower limit
-// else if (AverageHumidity>=630 && AverageHumidity<800){ 
-else if (humidityValue1 >= 630 && humidityValue1 < 800 || humidityValue2 < 630 && humidityValue2 < 800) { // Use 2 sensor reading 
-lcd.print(" MOIST");
-lcd.setRGB(0, 254, 0); //Green background
-digitalWrite(relayPin, LOW); // turn relay off:
-Serial.print("Pump stopped");
-}
+  //Main loop
+  //if (AverageHumidity<630) {- CLEAN UP
+  if (humidityValue1 > 600 || humidityValue2 > 600) {
+    lcd.print("   DRY");
+    lcd.setRGB(200, 0, 0); // Red background
+    digitalWrite(relayPin, HIGH); // turn relay on (water pump is on)
+    Serial.print("Pump is working");
+    delay(20000); //pump is working for 20 sec, loop delayed
+    Serial.print("Pump was working for 20 sec");
+  }
+  // You need to experiment with soil humidity sensor readings. In my case DRY more than 600; MOIST 400-600; FLOOD less then 400.
+  // else if (AverageHumidity>=630 && AverageHumidity<800){ - CLEAN UP
+  else if (humidityValue1 > 400 && humidityValue1 <= 600 || humidityValue2 > 400 && humidityValue2 <= 600) { // Use 2 sensor reading
+    lcd.print(" MOIST");
+    lcd.setRGB(0, 254, 0); //Green background
+    digitalWrite(relayPin, LOW); // turn relay off:
+    Serial.print("Pump stopped");
+  }
 
-else{
-lcd.print(" FLOOD");
-lcd.setRGB(0, 0, 250); //Blue background
-digitalWrite(relayPin, LOW); // turn relay off:
-}
+  else {
+    lcd.print(" FLOOD");
+    lcd.setRGB(0, 0, 250); //Blue background
+    digitalWrite(relayPin, LOW); // turn relay off:
+  }
 
-delay(500);
+  delay(500);
 
 }
